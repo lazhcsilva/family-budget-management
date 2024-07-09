ee1606b1ebe3487ac4164c3134eb54629751bde6
Diogo Peixoto <peixotoo@gmail.com>
2024-07-09 17:56:16 -0400

    feat: create bootstrap gradle project

---
+++ src/main/java/com/fbm/lazhcsilva/FbmApplication.java
@@ -0,0 +1,13 @@
+package com.fbm.lazhcsilva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbmApplication.class, args);
+	}
+
+}
