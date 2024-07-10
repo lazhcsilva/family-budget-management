527bcf436bbfba80ff579c915d59603deb49b52b
Diogo Peixoto <peixotoo@gmail.com>
2024-07-10 19:39:50 -0400

    feat: add static code analyzers (spotbugs, errorprone, and pmd)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.3.1"
@@ -6,6 +8,11 @@ plugins {
 	// Code formatter
 	id("com.palantir.git-version") version "3.1.0"
 	id("com.diffplug.spotless") version "6.25.0"
+
+	// Static code analyzer
+	id("net.ltgt.errorprone") version "4.0.1"
+	id("com.github.spotbugs") version "6.0.18"
+	pmd
 }
 
 group = "com.fbm.lazhcsilva"
@@ -24,10 +31,16 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+
 	runtimeOnly("com.h2database:h2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	errorprone("com.google.errorprone:error_prone_core:2.28.0")
 }
+
+// Code Formatter
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
 	java {
@@ -36,6 +49,19 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
 		palantirJavaFormat().formatJavadoc(true)
 		formatAnnotations()
 	}
+}
+
+// Static Code Analyzer
+tasks.withType<JavaCompile>().configureEach {
+	options.errorprone.disableWarningsInGeneratedCode.set(true)
+}
+
+pmd {
+	isIgnoreFailures = false
+}
+
+spotbugs {
+	ignoreFailures = false
 }
 
 tasks.withType<Test> {
