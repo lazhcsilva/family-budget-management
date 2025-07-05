4d5702c2e93ffb8827ad3566a9bd7824cb61e0dd
Lázaro Silva <lazhcsilva@gmail.com>
2025-07-05 17:52:40 -0300

    Update plugins version

---
+++ src/main/java/com/fbm/lazhcsilva/domain/user/model/User.java
@@ -0,0 +1,75 @@
+package com.fbm.lazhcsilva.domain.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String firstName;
+
+    @Column(length = 50, nullable = false)
+    private String lastName;
+
+    @Column(length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String password;
+
+    public User() {}
+
+    public User(Long id, String firstName, String lastName, String email, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    @Column(length = 50, nullable = false)
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
